7d4fa9f8409199d1ac26039f381db2c4f225af1d
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-02-22 23:36:02 +0700

    featured updateMJP

--- src/controllers/MajorInPlanSJGCtrl.js
+++ src/controllers/MajorInPlanSJGCtrl.js
@@ -12,7 +12,6 @@ const createMJP = asyncHandler(async (req, res) => {
   }
 });
 
-
 const getaMJP = asyncHandler(async (req, res) => {
   const { id } = req.params;
   validateMongoDbId(id);
@@ -25,22 +24,36 @@ const getaMJP = asyncHandler(async (req, res) => {
 });
 
 const getAllMJP = asyncHandler(async (req, res) => {
-    try {
-      let query = {};
-      // Xử lý điều kiện tìm kiếm theo trường 'name'
-      if (req.query.name) {
-        query.name = { $regex: req.query.name, $options: "i" }; // Tìm kiếm tất cả các mục có 'name' chứa chuỗi đã cung cấp (không phân biệt chữ hoa chữ thường)
-      }
-      const getAllMJP = await majorInPlanSJG.find(query);
-      res.json(getAllMJP);
-    } catch (error) {
-      throw new Error(error);
+  try {
+    let query = {};
+    // Xử lý điều kiện tìm kiếm theo trường 'name'
+    if (req.query.name) {
+      query.name = { $regex: req.query.name, $options: "i" }; // Tìm kiếm tất cả các mục có 'name' chứa chuỗi đã cung cấp (không phân biệt chữ hoa chữ thường)
     }
-  });
+    const getAllMJP = await majorInPlanSJG.find(query);
+    res.json(getAllMJP);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const updatedSMJP = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const updatedSMJP = await majorInPlanSJG.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(updatedSMJP);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
 module.exports = {
   createMJP,
   getaMJP,
   getAllMJP,
-  updatedSubject,
+  updatedSMJP,
   deletedCSubject,
 };
