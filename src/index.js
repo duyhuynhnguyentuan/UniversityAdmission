c19faac56d995c52817678b6a153694f32348570
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-02-23 16:42:33 +0700

    router studypofile

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require("body-parser");
 const dotenv = require('dotenv').config();
 const app = express();
 const port = 8000;
 const dbConnect = require('./config/database');
+const studyprofile = require('./routers/studyProfile');
 
 dbConnect();
 app.use(morgan('dev'))
 app.use(express.json())
+app.use(bodyParser.json());
+
+app.use("/api/v1/studyprofile", studyprofile);
+
 
 app.listen(port, ()=>{
     console.log(`App listening on port http://localhost:${port}`)
