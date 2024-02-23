8025091961de15901558f586c4029d74745e3fa3
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-02-23 11:30:49 +0700

    featured createnewStudyProfile

--- src/controllers/studyProfileCtrl.js
+++ src/controllers/studyProfileCtrl.js
@@ -1,7 +1,17 @@
-const StudyProfile  = require('..//models/studyProfile');
+const StudyProfile = require("../models/studyProfile");
 const asyncHandler = require("express-async-handler");
 const validateMongoDbId = require("../utils/validateMongoDbId");
 
+// create a new StudyProfile
+const newStudyProfile = asyncHandler(async (req, res, next) => {
+  try {
+    const newStudyProfile = await Subject.create(req.body);
+    res.json(newStudyProfile);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
 module.exports = {
-    
-}
+  newStudyProfile,
+};
