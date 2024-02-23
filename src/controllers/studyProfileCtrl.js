3010e59eaa1d200b5b8e917e8a4aa2de1d0357bb
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-02-23 11:37:58 +0700

    featured getAllStudyProfile

--- src/controllers/studyProfileCtrl.js
+++ src/controllers/studyProfileCtrl.js
@@ -13,16 +13,37 @@ const newStudyProfile = asyncHandler(async (req, res, next) => {
 });
 
 const getaStudyProfile = asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    validateMongoDbId(id);
-    try {
-      const getaStudyProfile = await Category.findById(id);
-      res.json(getaStudyProfile);
-    } catch (error) {
-      throw new Error(error);
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const getaStudyProfile = await Category.findById(id);
+    res.json(getaStudyProfile);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const getAllStudyProfile = asyncHandler(async (req, res) => {
+  try {
+    let query = {};
+    if (req.query.grade) {
+      query.grade = req.query.grade;
     }
-  });
+    if (req.query.grade_lt) {
+      query.grade = { $lt: req.query.grade_lt };
+    }
+    if (req.query.grade_gt) {
+      query.grade = { $gt: req.query.grade_lt };
+    }
+    const getAllStudyProfile = await Category.find(query);
+    res.json(getAllStudyProfile);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
 module.exports = {
   newStudyProfile,
-  getaStudyProfile
+  getaStudyProfile,
+  getAllStudyProfile,
 };
