aa6a6f8ad2caad2d1f8a47473c433f99878c3bff
duyhuynhnguyentuan <andyhntd2003@gmail.com>
2024-02-23 22:04:23 +0700

    solving retry

--- src/index.js
+++ src/index.js
@@ -16,7 +16,11 @@ const studyPSJ = require('./routers/StudyProfileSJ');
 const mJP = require('./routers/MajorInPlanSJG');
 const subject = require('./routers/Subject');
 const subjectG = require('./routers/SubjectG');
-
+const universityRouter  = require('./routers/university');
+const admissionPlanRouter  = require('./routers/AdmissionPlan');
+const provinceRouter = require('./routers/province');
+const schoolYear = require('./routers/SchoolYear');
+const University_Province = require('./routers/University_Province');
 
 dbConnect();
 app.use(morgan('dev'))
@@ -37,6 +41,13 @@ app.use("/api/v1/highSchool", highSchoolRouter)
 app.use("/api/v1/mainSubject", mainSubjectRouter)
 app.use("/api/v1/certificate", certificateRouter)
 app.use("/api/v1/certificateType", certificateTypeRouter)
+
+
+app.use("api/v1/university", universityRouter);
+app.use("api/v1/admissionPlan", admissionPlanRouter);
+app.use("api/v1/province", provinceRouter);
+app.use("api/v1/schoolYear", schoolYear);
+app.use("api/v1/University_Province", University_Province);
 app.listen(port, ()=>{
     console.log(`App listening on port http://localhost:${port}`)
 })
