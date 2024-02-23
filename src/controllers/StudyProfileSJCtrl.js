3559b77709b91fb2f2bd4db479c155e2b416ea47
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-02-23 16:35:06 +0700

    featured uploadExcel

--- src/controllers/StudyProfileSJCtrl.js
+++ src/controllers/StudyProfileSJCtrl.js
@@ -1,6 +1,8 @@
 const StudyProfileSJ = require("../models/studyProfileSubject");
 const asyncHandler = require("express-async-handler");
 const validateMongoDbId = require("../utils/validateMongoDbId");
+const XLSX = require("xlsx");
+const multer = require("multer");
 
 const createStudyProfileSJ = asyncHandler(async (req, res) => {
   try {
@@ -31,9 +33,36 @@ const getAllStudyProfileSJ = asyncHandler(async (req, res) => {
   }
 });
 
+const uploadXLSX = async (req, res) => {
+  try {
+    const path = req.file.path;
+    const workbook = XLSX.readFile(path);
+    const sheetNameList = workbook.SheetNames;
+    const jsonData = XLSX.utils.sheet_to_json(
+      workbook.Sheets[sheetNameList[0]]
+    );
+
+    if (jsonData.length === 0) {
+      return res.status(400).json({
+        success: false,
+        message: "Excel sheet has no data",
+      });
+    }
+
+    const savedData = await StudyProfileSJ.create(jsonData);
+
+    return res.status(201).json({
+      success: true,
+      message: `${savedData.length} rows added to the database`,
+    });
+  } catch (err) {
+    return res.status(500).json({ success: false, message: err.message });
+  }
+};
+
 module.exports = {
   createStudyProfileSJ,
   getaStudyProfileSJ,
   getAllStudyProfileSJ,
-  
+  uploadXLSX
 };
