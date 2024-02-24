6ea7da83b20301bfdf05a209f511d105d3be311e
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-02-24 08:08:26 +0700

    update server

--- src/index.js
+++ src/index.js
@@ -16,7 +16,6 @@ const studyPSJ = require('./routers/StudyProfileSJ');
 const mJP = require('./routers/MajorInPlanSJG');
 const subject = require('./routers/Subject');
 const subjectG = require('./routers/SubjectG');
-
 const universityRouter  = require('./routers/university');
 const admissionPlanRouter  = require('./routers/AdmissionPlan');
 const provinceRouter = require('./routers/province');
@@ -31,26 +30,24 @@ app.use(express.json())
 app.use(bodyParser.json());
 
 
+app.use("/api/v1/auth", authRouter);
 app.use("api/v1/university", universityRouter);
 app.use("api/v1/admissionPlan", admissionPlanRouter);
 app.use("api/v1/province", provinceRouter);
 app.use("api/v1/schoolYear", schoolYear);
 app.use("api/v1/University_Province", University_Province);
-
-app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/studyprofile", studyprofile);
 app.use("/api/v1/stdpsj", studyPSJ);
 app.use("/api/v1/mjp", mJP);
 app.use("/api/v1/subject", subject);
 app.use("/api/v1/subjectg", subjectG)
-
-
-app.use(notFound);
-app.use(errorHandler);
 app.use("/api/v1/highSchool", highSchoolRouter)
 app.use("/api/v1/mainSubject", mainSubjectRouter)
 app.use("/api/v1/certificate", certificateRouter)
 app.use("/api/v1/certificateType", certificateTypeRouter)
+
+app.use(notFound);
+app.use(errorHandler);
 app.listen(port, ()=>{
     console.log(`App listening on port http://localhost:${port}`)
 })
