0962e4b7995252fd567626ad152fc385413d5728
Nguyen Anh <ntac1k25@gmail.com>
2024-02-24 08:28:30 +0700

    CRUD +admission form +admission_methods +major in plan +major +formal major

--- src/index.js
+++ src/index.js
@@ -22,6 +22,11 @@ const provinceRouter = require('./routers/province');
 const schoolYear = require('./routers/SchoolYear');
 const University_Province = require('./routers/University_Province');
 
+const admissionForm = require('./routers/admissionForm');
+const admissionMethod = require('./routers/admissionMethod');
+const majorInPlan = require('./routers/majorInPlan');
+const major = require('./routers/Major');
+const formalMajor = require('./routers/formalMajor');
 
 
 dbConnect();
@@ -40,6 +45,12 @@ app.use("/api/v1/studyprofile", studyprofile);
 app.use("/api/v1/stdpsj", studyPSJ);
 app.use("/api/v1/mjp", mJP);
 app.use("/api/v1/subject", subject);
+app.use("/api/v1/subjectg", subjectG);
+app.use("/api/v1/admissionForm", admissionForm);
+app.use("/api/v1/admissionMethod", admissionMethod);
+app.use("/api/v1/majorInPlan", majorInPlan);
+app.use("/api/v1/major", major);
+app.use("/api/v1/formalMajor", formalMajor);
 app.use("/api/v1/subjectg", subjectG)
 app.use("/api/v1/highSchool", highSchoolRouter)
 app.use("/api/v1/mainSubject", mainSubjectRouter)
@@ -48,6 +59,6 @@ app.use("/api/v1/certificateType", certificateTypeRouter)
 
 app.use(notFound);
 app.use(errorHandler);
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`App listening on port http://localhost:${port}`)
 })
