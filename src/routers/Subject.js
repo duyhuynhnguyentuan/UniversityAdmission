238bfd701622169b0ac8e641f1b5816e47b09894
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-03-03 10:01:48 +0700

    config route swagger

--- src/routers/Subject.js
+++ src/routers/Subject.js
@@ -8,11 +8,152 @@ const {
 } = require('../controllers/SubjectCtrl');
 const router = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Subjects:
+ *       type: object
+ *       required:
+ *         - name
+ *         - score
+ *         - description
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: The auto-generated id of the book
+ *         score:
+ *           type: number
+ *           description: The book grade
+ *         description:
+ *           type: string
+ *           description: The Subjects profile
+ *         
+
+ */
+ /**
+  * @swagger
+  * tags:
+  *   name: Subjects
+  *   description: The Subjects managing API
+  */
+/**
+ * @swagger
+ * /api/v1/subject:
+ *   post:
+ *     summary: Create a new Subjects
+ *     tags: [Subjects]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Subjects'
+ *     responses:
+ *       200:
+ *         description: The Subjects was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Subjects'
+ *       500:
+ *         description: Some server error
+ */
 
 router.post("/", createSubject);
+/**
+ * @swagger
+ * /api/v1/subject/{id}:
+ *   get:
+ *     summary: Get the SubjectsSubject by id
+ *     tags: [SubjectsSubject]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The SubjectsSubject id
+ *     responses:
+ *       200:
+ *         description: The SubjectsSubject description by id
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/SubjectsSubject'
+ *       404:
+ *         description: The SubjectsSubject was not found
+ */
 router.get("/:id", getaSubject);
+/**
+ * @swagger
+ * /api/v1/subject:
+ *   get:
+ *     summary: Returns the list of all the SubjectsSubject
+ *     tags: [SubjectsSubject]
+ *     responses:
+ *       200:
+ *         description: The list of the SubjectsSubject
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/SubjectsSubject'
+ */
 router.get("/", getAllSubject);
+/**
+ * @swagger
+ * /api/v1/subject/{id}:
+ *  put:
+ *    summary: Update the Subjects by the id
+ *    tags: [Subjects]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The Subjects id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Subjects'
+ *    responses:
+ *      200:
+ *        description: The Subjects was updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Subjects'
+ *      404:
+ *        description: The Subjects was not found
+ *      500:
+ *        description: Some error happened
+ */
 router.put("/:id", updatedSubject);
+/**
+ * @swagger
+ * /api/v1/subject/{id}:
+ *   delete:
+ *     summary: Remove the Subjects by id
+ *     tags: [Subjects]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The Subjects id
+ * 
+ *     responses:
+ *       200:
+ *         description: The Subjects was deleted
+ *       404:
+ *         description: The Subjects was not found
+ */
 router.delete("/:id",deletedCSubject);
 
 module.exports = router;
--- src/routers/studyProfile.js
+++ src/routers/studyProfile.js
@@ -8,11 +8,160 @@ const {
 } = require('../controllers/studyProfileCtrl');
 const router = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     StudyProfile:
+ *       type: object
+ *       required:
+ *         - grade
+ *         - profile
+ *         - highschool
+ *         - schoolyear
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the book
+ *         grade:
+ *           type: string
+ *           description: The book grade
+ *         profile:
+ *           type: object
+ *           description: The studyProfile profile
+ *         highschool:
+ *          type: object
+ *          description: The highschool profile
+ *         schoolyear:
+ *          type: object
+ *          description: The schoolyear profile
 
+ */
+ /**
+  * @swagger
+  * tags:
+  *   name: StudyProfile
+  *   description: The StudyProfile managing API
+  */
+/**
+ * @swagger
+ * /api/v1/studyprofile:
+ *   post:
+ *     summary: Create a new studyprofile
+ *     tags: [Studyprofile]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Studyprofile'
+ *     responses:
+ *       200:
+ *         description: The Studyprofile was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Studyprofile'
+ *       500:
+ *         description: Some server error
+ */
 router.post("/", newStudyProfile);
+/**
+ * @swagger
+ * /api/v1/studyprofile/{id}:
+ *   get:
+ *     summary: Get the studyprofile by id
+ *     tags: [studyprofile]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The studyprofile id
+ *     responses:
+ *       200:
+ *         description: The studyprofile description by id
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Studyprofile'
+ *       404:
+ *         description: The studyprofile was not found
+ */
 router.get("/:id", getaStudyProfile);
+/**
+ * @swagger
+ * /api/v1/studyprofile:
+ *   get:
+ *     summary: Returns the list of all the StudyProfile
+ *     tags: [StudyProfile]
+ *     responses:
+ *       200:
+ *         description: The list of the StudyProfile
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/StudyProfile'
+ */
 router.get("/", getAllStudyProfile);
+
+/**
+ * @swagger
+ * /api/v1/studyprofile/{id}:
+ *  put:
+ *    summary: Update the book by the id
+ *    tags: [StudyProfile]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The StudyProfile id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/StudyProfile'
+ *    responses:
+ *      200:
+ *        description: The StudyProfile was updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/StudyProfile'
+ *      404:
+ *        description: The StudyProfile was not found
+ *      500:
+ *        description: Some error happened
+ */
 router.put("/:id", updatedStudyProfile);
+
+/**
+ * @swagger
+ * /api/v1/studyprofile/{id}:
+ *   delete:
+ *     summary: Remove the studyprofile by id
+ *     tags: [Studyprofile]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The studyprofile id
+ * 
+ *     responses:
+ *       200:
+ *         description: The studyprofile was deleted
+ *       404:
+ *         description: The studyprofile was not found
+ */
+
 router.delete("/:id",deletedStudyProfile);
 
 module.exports = router;
