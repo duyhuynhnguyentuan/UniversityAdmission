145b65bfad5515cd43e36aba543fa93b9c75124a
00stickman123 <00buom2017@gmail.com>
2024-03-05 15:29:50 +0700

    add swagger

--- src/routers/Major.js
+++ src/routers/Major.js
@@ -8,11 +8,129 @@ const {
 } = require("../controllers/Major")
 const router = express.Router();
 
-
+/** 
+ * @swagger
+ * /api/v1/major:
+ *   post:
+ *     tags:
+ *       - Major routes
+ *     summary: Create a new Major
+ *     description: Create a new Major
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Major'
+ *     responses:
+ *       '201':
+ *         description: A new Major has been created 
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/MajorResponse' 
+ */
 router.post('/', createMajor);
+/**
+ * @swagger
+ * /api/v1/major:
+ *   get:
+ *     tags:
+ *       - Major routes
+ *     summary: Get all the Major
+ *     description: Get information about all fetched major
+ *     responses:
+ *       200:
+ *         description: Major fetched successfully 
+ */
 router.get('/', getAllMajors);
+/** 
+ * @swagger
+ * /api/v1/major/{id}:
+ *   get:
+ *     tags:
+ *       - Major routes
+ *     summary: Get a specific Highschool
+ *     description: Get information about a specific Major
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The id of the University
+ *     responses: 
+ *       '200':   
+ *         description: Highschool fetched successfully
+ *         content:
+ *           application/json:
+ *             schema: 
+ *                $ref: '#/components/schemas/MajorResponse'
+ *       '404':
+ *         description: Highschool not found 
+ */
 router.get('/:id', getMajorById);
+/** 
+ * @swagger
+ * /api/v1/major/{id}:
+ *   put:
+ *     tags:
+ *       - Major routes
+ *     summary: Update a single Major
+ *     description: Update a specific Major
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The id of the Major being updated
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Major'
+ *     responses:
+ *       '200':
+ *         description: A university has been updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/MajorResponse'
+ *       '403':
+ *         description: Forbidden
+ *       '404':
+ *         description: No Major found 
+ */
 router.put('/:id', updateMajor);
+/** 
+ * @swagger
+ * /api/v1/major/{id}:
+ *   delete:
+ *     tags:
+ *       - Major routes
+ *     summary: Delete a single Major
+ *     description: Delete a specific Major
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The id of the Major being deleted
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Major'
+ *     responses:
+ *       '200':
+ *         description: A Major has been deleted successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/MajorResponse'
+ *       '403':
+ *         description: Forbidden
+ *       '404':
+ *         description: No Major found 
+ */
 router.delete('/:id', deleteMajor);
 
 module.exports = router;
--- src/routers/majorInPlan.js
+++ src/routers/majorInPlan.js
@@ -8,11 +8,129 @@ const {
 } = require("../controllers/MajorInPlanCtrl")
 const router = express.Router();
 
-
+/** 
+ * @swagger
+ * /api/v1/majorInPlan:
+ *   post:
+ *     tags:
+ *       - MajorInPlan routes
+ *     summary: Create a new MajorInPlan
+ *     description: Create a new MajorInPlan
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/MajorInPlan'
+ *     responses:
+ *       '201':
+ *         description: A new MajorInPlan has been created 
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/MajorInPlanResponse' 
+ */
 router.post('/',createMajorInPlan);
+/**
+ * @swagger
+ * /api/v1/majorInPlan:
+ *   get:
+ *     tags:
+ *       - MajorInPlan routes
+ *     summary: Get all the MajorInPlan
+ *     description: Get information about all fetched majorInPlan
+ *     responses:
+ *       200:
+ *         description: MajorInPlan fetched successfully 
+ */
 router.get('/',getAllMajorInPlans);
+/** 
+ * @swagger
+ * /api/v1/majorInPlan/{id}:
+ *   get:
+ *     tags:
+ *       - MajorInPlan routes
+ *     summary: Get a specific Highschool
+ *     description: Get information about a specific MajorInPlan
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The id of the University
+ *     responses: 
+ *       '200':   
+ *         description: Highschool fetched successfully
+ *         content:
+ *           application/json:
+ *             schema: 
+ *                $ref: '#/components/schemas/MajorInPlanResponse'
+ *       '404':
+ *         description: Highschool not found 
+ */
 router.get('/:id',getMajorInPlanById);
+/** 
+ * @swagger
+ * /api/v1/majorInPlan/{id}:
+ *   put:
+ *     tags:
+ *       - MajorInPlan routes
+ *     summary: Update a single MajorInPlan
+ *     description: Update a specific MajorInPlan
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The id of the MajorInPlan being updated
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/MajorInPlan'
+ *     responses:
+ *       '200':
+ *         description: A university has been updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/MajorInPlanResponse'
+ *       '403':
+ *         description: Forbidden
+ *       '404':
+ *         description: No MajorInPlan found 
+ */
 router.put('/:id',updateMajorInPlan);
+/** 
+ * @swagger
+ * /api/v1/majorInPlan/{id}:
+ *   delete:
+ *     tags:
+ *       - MajorInPlan routes
+ *     summary: Delete a single MajorInPlan
+ *     description: Delete a specific MajorInPlan
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The id of the MajorInPlan being deleted
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/MajorInPlan'
+ *     responses:
+ *       '200':
+ *         description: A MajorInPlan has been deleted successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/MajorInPlanResponse'
+ *       '403':
+ *         description: Forbidden
+ *       '404':
+ *         description: No MajorInPlan found 
+ */
 router.delete('/:id',deleteMajorInPlan);
 
 module.exports = router;
