95ef7f3cbf969c44192d35dfa0637a4d30ac8b5e
duyhuynhnguyentuan <andyhntd2003@gmail.com>
2024-03-07 18:39:51 +0700

    update api documentation

--- src/models/admissionPlan.js
+++ src/models/admissionPlan.js
@@ -1,5 +1,77 @@
 const mongoose = require('mongoose')
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     AdmissionPlan:
+ *       type: object 
+ *       required: 
+ *         - schoolYear
+ *         - planDescription
+ *       properties:
+ *         schoolYear: 
+ *           type: array
+ *           items:
+ *             $ref: '#/components/schemas/schoolYear'
+ *           description: Array of School Year objects associated with the admission plan.
+ *         planDescription: 
+ *           type: string
+ *           required: true
+ *           description: Description of the admission plan.
+ *         university: 
+ *           type: array
+ *           items:
+ *             $ref: '#/components/schemas/university'
+ *           description: Array of University objects associated with the admission plan.
+ *         majorInPlan: 
+ *           type: array
+ *           items:
+ *             $ref: '#/components/schemas/majorInPlan'
+ *           description: Array of MajorInPlan objects associated with the admission plan.
+ *         createdAt:
+ *           type: string
+ *           format: date-time
+ *           description: The date and time when the admission plan was created.
+ *         updatedAt:
+ *           type: string
+ *           format: date-time
+ *           description: The date and time when the admission plan was last updated.
+ * 
+ *     AdmissionPlanResponse:
+ *       type: object
+ *       properties:
+ *         _id:
+ *           type: string
+ *           description: The unique identifier for the admission plan.
+ *         schoolYear: 
+ *           type: array
+ *           items:
+ *             $ref: '#/components/schemas/schoolYear'
+ *           description: Array of School Year objects associated with the admission plan.
+ *         planDescription: 
+ *           type: string
+ *           description: Description of the admission plan.
+ *         university: 
+ *           type: array
+ *           items:
+ *             $ref: '#/components/schemas/university'
+ *           description: Array of University objects associated with the admission plan.
+ *         majorInPlan: 
+ *           type: array
+ *           items:
+ *             $ref: '#/components/schemas/majorInPlan'
+ *           description: Array of MajorInPlan objects associated with the admission plan.
+ *         createdAt:
+ *           type: string
+ *           format: date-time
+ *           description: The date and time when the admission plan was created.
+ *         updatedAt:
+ *           type: string
+ *           format: date-time
+ *           description: The date and time when the admission plan was last updated.
+ */
+
 const admissionPlanSchema = new mongoose.Schema(
   { 
     schoolYear: [{
